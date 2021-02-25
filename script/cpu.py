88ea09f4fcfe5ff62599ddc6edaa9a776ca28c9c
peihe <peihe@yuntongxun.com>
2021-02-25 11:47:19 +0800

    cpuinfo

---
+++ script/cpu.py
@@ -0,0 +1,19 @@
+#!/usr/bin env python3
+
+import psutil
+
+def memissue():
+    print("内存信息:")
+    mem = psutil.virtual_memory()
+    # 单位换算为MB
+    memtotal = mem.total / 1024 / 1024
+    memused = mem.used / 1024 / 1024
+    mempercentage = str(mem.used / mem.total * 100) + '%'
+
+    print("Used: %.3fMB" % memused)
+    print("Total: %.3fMB" % memtotal)
+    print("Percentage: %.3f" % mempercentage)
+    
+memissue()
+
+
