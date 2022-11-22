c7136d3d37644726f032db6af15494a607479244
Matt.Pei <sirpei@163.com>
2022-11-22 11:22:21 +0800

    elastic script

--- docs/elastic/elastic_index_backup.sh
+++ docs/elastic/elastic_index_backup.sh
@@ -1,58 +1,113 @@
 #!/bin/bash
-DATA_DIR=/efs/elastic
-S3_DIR=
+INDEX=(hdms_nginxlog hdms_cacenter_test com.navinfo.imp.id hdms_task)
+Month=`date +%B`
 YESTERDAY=`date -d '-1 day' +%Y%m%d`
 INDEX_TIME=`date -d '-1 day' +%Y-%m-%d`
-#INDEX=(hdms_nginxlog hdms_cacenter_test com.navinfo.imp.id hdms_task)
-INDEX=(hdms_nginxlog)
+IpAddress=10.9.4.86
+# INDEX=(hdms_nginxlog)
 
 
-esStatus=$(curl -I -sL -m 5  http://10.9.4.86:9200 | grep 200)
-if [[ "${esStatus}" != "" ]]; then
-    echo "ES Status: ${esStatus}"
-    # 判断备份目录是否存在
-    #for i in ${INDEX[@]}
-    #do
-    #    if [[ ! -d ${DATA_DIR}/${i}-${YESTERDAY} ]]; then
-    #        mkdir -pv ${DATA_DIR}/${i}-${YESTERDAY}
-    #    fi
-    #done
-    # 创建仓库
+function repository_status() {
+    res=`echo $?`
+    if [[ $res != "0" ]]; then
+        echo "ES repository fails to be created"
+    else
+        echo "ES repository is successfully created"
+    fi
+}
+
+function back_status() {
+    res=`echo $?`
+    if [[ $res != "0" ]]; then
+        echo "ES Index backup failed"
+    else
+        echo "ES Index backup succeeded"
+    fi
+}
+
+
+ESStatus=$(curl -I -sL -m 5  http://${IpAddress}:9200 | grep '200')
+if [[ "${ESStatus}" != "" ]]; then
+    echo "ES Status: ${ESStatus}"
+    # 创建s3存储桶目录文件
     for i in ${INDEX[@]}
     do
-    # 创建备份仓库
-    curl -H "Content-Type: application/json" -XPUT http://10.9.4.86:9200/_snapshot/${i}-${YESTERDAY} -d '
-    {
-        "type": "s3",
-        "settings": {
-            "bucket": "data-backup-hdms-cn-northwest",
-            "region": "cn-northwest-1",
-            "base_path": "'${i}'-'${YESTERDAY}'",
-            "endpoint": "s3.cn-northwest-1.amazonaws.com.cn"
-        }
-    }'
-    res=`echo $?`
-    if [[ $res != "1" ]]; then
-        echo "es仓库创建失败"
-    else
-        echo "es仓库创建成功"
-        # for i in ${INDEX[@]}
-        # do
-        # 备份索引
-        curl -H "Content-Type: application/json" -XPUT http://10.9.4.86:9200/_snapshot/${i}-${YESTERDAY}/${i}-${YESTERDAY}?wait_for_completion=true -d '
+        # 循环创建仓库
+        curl -H "Content-Type: application/json" -XPUT http://${IpAddress}:9200/_snapshot/${i}-${YESTERDAY} -d '
         {
-            "indices": "'${i}'_'${INDEX_TIME}'"
+            "type": "s3",
+            "settings": {
+                "bucket": "data-backup-hdms-cn-northwest",
+                "region": "cn-northwest-1",
+                "base_path": "'${i}'/'${i}'-'${YESTERDAY}'/",
+                "endpoint": "s3.cn-northwest-1.amazonaws.com.cn"
+            }
         }'
-        # done
-    fi
+        # Example: hdms_nginxlog-20220906/hdms_cacenter_test-20220906/hdms_task-20220906/com.navinfo.imp.id-20220906
+        aws s3api put-object --bucket data-backup-hdms-cn-northwest --key ${i}/${Month}/${i}-${YESTERDAY}/
+        # 创建备份仓库
+        if [[ "${i}" == "hdms_nginxlog" ]]; then
+            curl -H "Content-Type: application/json" -XPUT http://${IpAddress}:9200/_snapshot/${i}-${YESTERDAY} -d '
+            {
+                "type": "s3",
+                "settings": {
+                    "bucket": "data-backup-hdms-cn-northwest",
+                    "region": "cn-northwest-1",
+                    "base_path": "'${i}'/'${i}'-'${YESTERDAY}'/",
+                    "endpoint": "s3.cn-northwest-1.amazonaws.com.cn"
+                }
+            }'
+            repository_status
+        elif [[ "${i}" == "hdms_cacenter_test" ]]; then
+            curl -H "Content-Type: application/json" -XPUT http://${IpAddress}:9200/_snapshot/${i}-${YESTERDAY} -d '
+            {
+                "type": "s3",
+                "settings": {
+                    "bucket": "data-backup-hdms-cn-northwest",
+                    "region": "cn-northwest-1",
+                    "base_path": "'${i}'/'${i}'-'${YESTERDAY}'/",
+                    "endpoint": "s3.cn-northwest-1.amazonaws.com.cn"
+                }
+            }'
+            repository_status
+        elif [[ "${i}" == "com.navinfo.imp.id" ]]; then
+            curl -H "Content-Type: application/json" -XPUT http://${IpAddress}:9200/_snapshot/${i}-${YESTERDAY} -d '
+            {
+                "type": "s3",
+                "settings": {
+                    "bucket": "data-backup-hdms-cn-northwest",
+                    "region": "cn-northwest-1",
+                    "base_path": "'${i}'/'${i}'-'${YESTERDAY}'/",
+                    "endpoint": "s3.cn-northwest-1.amazonaws.com.cn"
+                }
+            }'
+            repository_status
+        elif [[ "${i}" == "hdms_task" ]]; then
+            curl -H "Content-Type: application/json" -XPUT http://${IpAddress}:9200/_snapshot/${i}-${YESTERDAY} -d '
+            {
+                "type": "s3",
+                "settings": {
+                    "bucket": "data-backup-hdms-cn-northwest",
+                    "region": "cn-northwest-1",
+                    "base_path": "'${i}'/'${i}'-'${YESTERDAY}'/",
+                    "endpoint": "s3.cn-northwest-1.amazonaws.com.cn"
+                }
+            }'
+            repository_status
+        else
+            echo "Invalid index name or created index name not found"
+        fi
+            # 备份索引
+            echo "Start backup"
+            curl -H "Content-Type: application/json" -XPUT http://${IpAddress}:9200/_snapshot/${i}-${YESTERDAY}/${i}-${YESTERDAY}?wait_for_completion=true -d '
+            {
+                "indices": "'${i}'_'${INDEX_TIME}'"
+            }'
+            back_status
     done
-
 else
     echo "ES Status: ${ESStatus}"
     exit 1
 fi
-
-
-
 
 
